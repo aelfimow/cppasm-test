ea9ced5ffdde85773038955bf69cecd4fb78741d
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-29 22:43:33 +0200

    Removed unused test case

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,6 @@
 #include <gtest/gtest.h>
 #include <cppasm.h>
 
-TEST(CppAsmTest, Section)
-{
-    section(".text");
-    section(".data");
-}
-
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
