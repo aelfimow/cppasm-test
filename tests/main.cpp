910096dcee2581e76a3fbfe72fd2ca0452d5056c
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-30 23:02:05 +0200

    Set streamdest buffer

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
 #include <cppasm.h>
+#include <streamdest.h>
+#include "streamdest_buf.h"
 
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+
+    asmout.set(new streamdest_buf);
+
     return RUN_ALL_TESTS();
 }
