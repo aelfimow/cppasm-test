449675f4833c3ee22a15ef1a8d02d002b4e2cae7
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-31 20:55:33 +0200

    Added function to get asm line

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,11 +3,24 @@
 #include <streamdest.h>
 #include "streamdest_buf.h"
 
+static streamdest_buf *test_buf = nullptr;
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
-    asmout.set(new streamdest_buf);
+    test_buf = new streamdest_buf;
+
+    asmout.set(test_buf);
 
     return RUN_ALL_TESTS();
+}
+
+std::string asmstr()
+{
+    const std::string str = test_buf->get();
+
+    test_buf->clear();
+
+    return str;
 }
