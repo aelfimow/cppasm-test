a1b884e5041eb8641ebd5eb5ef23ed5821e8b7bc
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-06 21:25:17 +0200

    Instruction5: test for Type3

--- tests/test_Instruction5.cpp
+++ tests/test_Instruction5.cpp
@@ -3,16 +3,6 @@
 
 extern std::string asmstr();
 
-static void gen_Instruction5_Type3(Instruction5_Type3 &instr)
-{
-    m8 addr { RDX };
-    instr(k1, k2);
-    instr(k1, addr);
-    instr(addr, k1);
-    instr(k1, EAX);
-    instr(EAX, k2);
-}
-
 static void gen_Instruction5_Type4(Instruction5_Type4 &instr)
 {
     m16 addr { RDX };
@@ -77,9 +67,24 @@ TEST(Instruction5, Type2)
     EXPECT_EQ(asmstr(), "pextrw $0xFF, %xmm0, (%eax)");
 }
 
+TEST(Instruction5, Type3)
+{
+    m8 addr { RDX };
+
+    KMOVB(k1, k2);
+    EXPECT_EQ(asmstr(), "kmovb %k2, %k1");
+    KMOVB(k1, addr);
+    EXPECT_EQ(asmstr(), "kmovb (%rdx), %k1");
+    KMOVB(addr, k1);
+    EXPECT_EQ(asmstr(), "kmovb %k1, (%rdx)");
+    KMOVB(k1, EAX);
+    EXPECT_EQ(asmstr(), "kmovb %eax, %k1");
+    KMOVB(EAX, k2);
+    EXPECT_EQ(asmstr(), "kmovb %k2, %eax");
+}
+
 TEST(Instruction5, AllTypes)
 {
-    gen_Instruction5_Type3(KMOVB);
     gen_Instruction5_Type4(KMOVW);
     gen_Instruction5_Type5(KMOVD);
     gen_Instruction5_Type6(KMOVQ);
