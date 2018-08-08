c7537c7ac558451ccc39162cb3d7f33e088d0cdd
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-08 21:50:10 +0200

    Instruction7: test for Type1

--- tests/test_Instruction7.cpp
+++ tests/test_Instruction7.cpp
@@ -3,19 +3,6 @@
 
 extern std::string asmstr();
 
-static void gen_Instruction7_Type1(Instruction7_Type1 &instr)
-{
-    m16 addr1 { RAX };
-    m32 addr2 { RBX };
-    m64 addr3 { RBX };
-    std::string loc { "gen_Instruction7_Type1_label" };
-    instr(loc);
-    instr(RAX);
-    instr(addr1);
-    instr(addr2);
-    instr(addr3);
-}
-
 static void gen_Instruction7_Type2(Instruction7_Type2 &instr)
 {
     m16 addr1 { RAX };
@@ -39,12 +26,56 @@ static void gen_Instruction7_Type3(Instruction7_Type3 &instr)
     instr(addr3);
 }
 
+TEST(Instruction7, Type1)
+{
+    m16 addr1 { EAX };
+    m32 addr2 { EBX };
+    m16 addr3 { RAX };
+    m32 addr4 { RBX };
+    m64 addr5 { RBX };
+    std::string loc { "label" };
+
+    JMP(loc);
+    EXPECT_EQ(asmstr(), "jmp label");
+    JMP(AX);
+    EXPECT_EQ(asmstr(), "jmp *%ax");
+    JMP(EAX);
+    EXPECT_EQ(asmstr(), "jmp *%eax");
+    JMP(RAX);
+    EXPECT_EQ(asmstr(), "jmp *%rax");
+    JMP(addr1);
+    EXPECT_EQ(asmstr(), "jmp *(%eax)");
+    JMP(addr2);
+    EXPECT_EQ(asmstr(), "jmp *(%ebx)");
+    JMP(addr3);
+    EXPECT_EQ(asmstr(), "jmp *(%rax)");
+    JMP(addr4);
+    EXPECT_EQ(asmstr(), "jmp *(%rbx)");
+    JMP(addr5);
+    EXPECT_EQ(asmstr(), "jmp *(%rbx)");
+
+    CALL(loc);
+    EXPECT_EQ(asmstr(), "call label");
+    CALL(AX);
+    EXPECT_EQ(asmstr(), "call *%ax");
+    CALL(EAX);
+    EXPECT_EQ(asmstr(), "call *%eax");
+    CALL(RAX);
+    EXPECT_EQ(asmstr(), "call *%rax");
+    CALL(addr1);
+    EXPECT_EQ(asmstr(), "call *(%eax)");
+    CALL(addr2);
+    EXPECT_EQ(asmstr(), "call *(%ebx)");
+    CALL(addr3);
+    EXPECT_EQ(asmstr(), "call *(%rax)");
+    CALL(addr4);
+    EXPECT_EQ(asmstr(), "call *(%rbx)");
+    CALL(addr5);
+    EXPECT_EQ(asmstr(), "call *(%rbx)");
+}
+
 TEST(Instruction7, AllTypes)
 {
-    label("gen_Instruction7_Type1_label");
-    gen_Instruction7_Type1(JMP);
-    gen_Instruction7_Type1(CALL);
-
     gen_Instruction7_Type2(POP);
 
     label("gen_Instruction7_Type3_label");
