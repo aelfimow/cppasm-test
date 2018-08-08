7e5344b5ca45a44cc2f4dbe5d962002e5c350dac
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-08 21:35:34 +0200

    Instruction7: added test file

---
+++ tests/test_Instruction7.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include <cppasm.h>
+
+extern std::string asmstr();
+
+static void gen_Instruction7_Type1(Instruction7_Type1 &instr)
+{
+    m16 addr1 { RAX };
+    m32 addr2 { RBX };
+    m64 addr3 { RBX };
+    std::string loc { "gen_Instruction7_Type1_label" };
+    instr(loc);
+    instr(RAX);
+    instr(addr1);
+    instr(addr2);
+    instr(addr3);
+}
+
+static void gen_Instruction7_Type2(Instruction7_Type2 &instr)
+{
+    m16 addr1 { RAX };
+    m64 addr2 { RCX };
+    instr(AX);
+    instr(addr1);
+    instr(RCX);
+    instr(addr2);
+    instr(FS);
+}
+
+static void gen_Instruction7_Type3(Instruction7_Type3 &instr)
+{
+    m16 addr1 { RAX };
+    m32 addr2 { RBX };
+    m64 addr3 { RBX };
+    std::string loc { "gen_Instruction7_Type3_label" };
+    instr(loc);
+    instr(addr1);
+    instr(addr2);
+    instr(addr3);
+}
+
+TEST(Instruction7, AllTypes)
+{
+    label("gen_Instruction7_Type1_label");
+    gen_Instruction7_Type1(JMP);
+    gen_Instruction7_Type1(CALL);
+
+    gen_Instruction7_Type2(POP);
+
+    label("gen_Instruction7_Type3_label");
+    gen_Instruction7_Type3(JMP_FAR);
+    gen_Instruction7_Type3(CALL_FAR);
+}
