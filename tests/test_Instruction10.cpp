ffc5b653ae34d920327f9a861457a1c816e9937b
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-12 20:34:41 +0200

    Instruction10: added tests

--- tests/test_Instruction10.cpp
+++ tests/test_Instruction10.cpp
@@ -3,42 +3,65 @@
 
 extern std::string asmstr();
 
-static void gen_Instruction10_Type1(Instruction10_Type1 &instr)
+TEST(Instruction10, Type1)
 {
-    instr(AX);
-    instr(RAX);
-    m16 addr1 { RDX };
-    m64 addr3 { RDX };
-    instr(addr1);
-    instr(addr3);
+    m16 addr1 { EDX };
+    m32 addr2 { EDX };
+    m16 addr3 { RDX };
+    m64 addr4 { RDX };
     imm8 v1 { 0xAA };
     imm16 v2 { 0xAABB };
-    instr(v1);
-    instr(v2);
-    instr(GS);
+    imm32 v3 { 0xAABBCCDD };
+
+    PUSH(AX);
+    EXPECT_EQ(asmstr(), "push %ax");
+    PUSH(EAX);
+    EXPECT_EQ(asmstr(), "push %eax");
+    PUSH(RAX);
+    EXPECT_EQ(asmstr(), "push %rax");
+    PUSH(addr1);
+    EXPECT_EQ(asmstr(), "pushw (%edx)");
+    PUSH(addr2);
+    EXPECT_EQ(asmstr(), "pushl (%edx)");
+    PUSH(addr3);
+    EXPECT_EQ(asmstr(), "pushw (%rdx)");
+    PUSH(addr4);
+    EXPECT_EQ(asmstr(), "pushq (%rdx)");
+    PUSH(v1);
+    EXPECT_EQ(asmstr(), "push $0xAA");
+    PUSH(v2);
+    EXPECT_EQ(asmstr(), "push $0xAABB");
+    PUSH(v3);
+    EXPECT_EQ(asmstr(), "push $0xAABBCCDD");
+    PUSH(GS);
+    EXPECT_EQ(asmstr(), "push %gs");
 }
 
-static void gen_Instruction10_Type2(Instruction10_Type2 &instr)
+TEST(Instruction10, Type2)
 {
     m8 addr1 { RDX };
     m16 addr2 { RDX };
     m32 addr3 { RDX };
     m64 addr4 { RDX };
-    instr(ECX, AL);
-    instr(ECX, addr1);
-    instr(ECX, AX);
-    instr(ECX, addr2);
-    instr(ECX, EAX);
-    instr(ECX, addr3);
-    instr(RCX, AL);
-    instr(RCX, addr1);
-    instr(RCX, RAX);
-    instr(RCX, addr4);
-}
 
-TEST(Instruction10, TypeX)
-{
-    gen_Instruction10_Type1(PUSH);
-
-    gen_Instruction10_Type2(CRC32);
+    CRC32(ECX, AL);
+    EXPECT_EQ(asmstr(), "crc32 %al, %ecx");
+    CRC32(ECX, addr1);
+    EXPECT_EQ(asmstr(), "crc32b (%rdx), %ecx");
+    CRC32(ECX, AX);
+    EXPECT_EQ(asmstr(), "crc32 %ax, %ecx");
+    CRC32(ECX, addr2);
+    EXPECT_EQ(asmstr(), "crc32w (%rdx), %ecx");
+    CRC32(ECX, EAX);
+    EXPECT_EQ(asmstr(), "crc32 %eax, %ecx");
+    CRC32(ECX, addr3);
+    EXPECT_EQ(asmstr(), "crc32l (%rdx), %ecx");
+    CRC32(RCX, AL);
+    EXPECT_EQ(asmstr(), "crc32 %al, %rcx");
+    CRC32(RCX, addr1);
+    EXPECT_EQ(asmstr(), "crc32b (%rdx), %rcx");
+    CRC32(RCX, RAX);
+    EXPECT_EQ(asmstr(), "crc32 %rax, %rcx");
+    CRC32(RCX, addr4);
+    EXPECT_EQ(asmstr(), "crc32q (%rdx), %rcx");
 }
