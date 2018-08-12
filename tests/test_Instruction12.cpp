b87fd0386817c062e78a5068545cac731b4f8e88
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-12 21:22:54 +0200

    Instruction12: test for Type1

--- tests/test_Instruction12.cpp
+++ tests/test_Instruction12.cpp
@@ -3,26 +3,6 @@
 
 extern std::string asmstr();
 
-static void gen_Instruction12_Type1(Instruction12_Type1 &instr)
-{
-    m16 addr1 { RDX };
-    m32 addr2 { RDX };
-    m64 addr3 { RDX };
-    imm8 value { 1 };
-    instr(AX, BX);
-    instr(addr1, AX);
-    instr(EAX, EBX);
-    instr(addr2, EAX);
-    instr(RAX, RBX);
-    instr(addr3, RAX);
-    instr(AX, value);
-    instr(EAX, value);
-    instr(RAX, value);
-    instr(addr1, value);
-    instr(addr2, value);
-    instr(addr3, value);
-}
-
 static void gen_Instruction12_Type2(Instruction12_Type2 &instr)
 {
     m8 addr1 { RDX };
@@ -79,13 +59,116 @@ static void gen_Instruction12_Type4(Instruction12_Type4 &instr)
     instr(addr3, RBX, CL);
 }
 
+TEST(Instruction12, Type1)
+{
+    m16 addr1 { RDX };
+    m32 addr2 { RDX };
+    m64 addr3 { RDX };
+    imm8 value { 1 };
+
+    BT(AX, BX);
+    EXPECT_EQ(asmstr(), "bt %bx, %ax");
+    BT(addr1, AX);
+    EXPECT_EQ(asmstr(), "bt %ax, (%rdx)");
+    BT(EAX, EBX);
+    EXPECT_EQ(asmstr(), "bt %ebx, %eax");
+    BT(addr2, EAX);
+    EXPECT_EQ(asmstr(), "bt %eax, (%rdx)");
+    BT(RAX, RBX);
+    EXPECT_EQ(asmstr(), "bt %rbx, %rax");
+    BT(addr3, RAX);
+    EXPECT_EQ(asmstr(), "bt %rax, (%rdx)");
+    BT(AX, value);
+    EXPECT_EQ(asmstr(), "bt $0x01, %ax");
+    BT(EAX, value);
+    EXPECT_EQ(asmstr(), "bt $0x01, %eax");
+    BT(RAX, value);
+    EXPECT_EQ(asmstr(), "bt $0x01, %rax");
+    BT(addr1, value);
+    EXPECT_EQ(asmstr(), "btw $0x01, (%rdx)");
+    BT(addr2, value);
+    EXPECT_EQ(asmstr(), "btl $0x01, (%rdx)");
+    BT(addr3, value);
+    EXPECT_EQ(asmstr(), "btq $0x01, (%rdx)");
+
+    BTC(AX, BX);
+    EXPECT_EQ(asmstr(), "btc %bx, %ax");
+    BTC(addr1, AX);
+    EXPECT_EQ(asmstr(), "btc %ax, (%rdx)");
+    BTC(EAX, EBX);
+    EXPECT_EQ(asmstr(), "btc %ebx, %eax");
+    BTC(addr2, EAX);
+    EXPECT_EQ(asmstr(), "btc %eax, (%rdx)");
+    BTC(RAX, RBX);
+    EXPECT_EQ(asmstr(), "btc %rbx, %rax");
+    BTC(addr3, RAX);
+    EXPECT_EQ(asmstr(), "btc %rax, (%rdx)");
+    BTC(AX, value);
+    EXPECT_EQ(asmstr(), "btc $0x01, %ax");
+    BTC(EAX, value);
+    EXPECT_EQ(asmstr(), "btc $0x01, %eax");
+    BTC(RAX, value);
+    EXPECT_EQ(asmstr(), "btc $0x01, %rax");
+    BTC(addr1, value);
+    EXPECT_EQ(asmstr(), "btcw $0x01, (%rdx)");
+    BTC(addr2, value);
+    EXPECT_EQ(asmstr(), "btcl $0x01, (%rdx)");
+    BTC(addr3, value);
+    EXPECT_EQ(asmstr(), "btcq $0x01, (%rdx)");
+
+    BTR(AX, BX);
+    EXPECT_EQ(asmstr(), "btr %bx, %ax");
+    BTR(addr1, AX);
+    EXPECT_EQ(asmstr(), "btr %ax, (%rdx)");
+    BTR(EAX, EBX);
+    EXPECT_EQ(asmstr(), "btr %ebx, %eax");
+    BTR(addr2, EAX);
+    EXPECT_EQ(asmstr(), "btr %eax, (%rdx)");
+    BTR(RAX, RBX);
+    EXPECT_EQ(asmstr(), "btr %rbx, %rax");
+    BTR(addr3, RAX);
+    EXPECT_EQ(asmstr(), "btr %rax, (%rdx)");
+    BTR(AX, value);
+    EXPECT_EQ(asmstr(), "btr $0x01, %ax");
+    BTR(EAX, value);
+    EXPECT_EQ(asmstr(), "btr $0x01, %eax");
+    BTR(RAX, value);
+    EXPECT_EQ(asmstr(), "btr $0x01, %rax");
+    BTR(addr1, value);
+    EXPECT_EQ(asmstr(), "btrw $0x01, (%rdx)");
+    BTR(addr2, value);
+    EXPECT_EQ(asmstr(), "btrl $0x01, (%rdx)");
+    BTR(addr3, value);
+    EXPECT_EQ(asmstr(), "btrq $0x01, (%rdx)");
+
+    BTS(AX, BX);
+    EXPECT_EQ(asmstr(), "bts %bx, %ax");
+    BTS(addr1, AX);
+    EXPECT_EQ(asmstr(), "bts %ax, (%rdx)");
+    BTS(EAX, EBX);
+    EXPECT_EQ(asmstr(), "bts %ebx, %eax");
+    BTS(addr2, EAX);
+    EXPECT_EQ(asmstr(), "bts %eax, (%rdx)");
+    BTS(RAX, RBX);
+    EXPECT_EQ(asmstr(), "bts %rbx, %rax");
+    BTS(addr3, RAX);
+    EXPECT_EQ(asmstr(), "bts %rax, (%rdx)");
+    BTS(AX, value);
+    EXPECT_EQ(asmstr(), "bts $0x01, %ax");
+    BTS(EAX, value);
+    EXPECT_EQ(asmstr(), "bts $0x01, %eax");
+    BTS(RAX, value);
+    EXPECT_EQ(asmstr(), "bts $0x01, %rax");
+    BTS(addr1, value);
+    EXPECT_EQ(asmstr(), "btsw $0x01, (%rdx)");
+    BTS(addr2, value);
+    EXPECT_EQ(asmstr(), "btsl $0x01, (%rdx)");
+    BTS(addr3, value);
+    EXPECT_EQ(asmstr(), "btsq $0x01, (%rdx)");
+}
+
 TEST(Instruction12, TypeX)
 {
-    gen_Instruction12_Type1(BT);
-    gen_Instruction12_Type1(BTC);
-    gen_Instruction12_Type1(BTR);
-    gen_Instruction12_Type1(BTS);
-
     gen_Instruction12_Type2(XCHG);
 
     gen_Instruction12_Type3(MOVSX);
