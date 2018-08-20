18b51fd5fb59616a688ed2b4578d7498dac6780b
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-20 22:38:07 +0200

    imm: added test for imm16

--- tests/test_imm.cpp
+++ tests/test_imm.cpp
@@ -6,8 +6,17 @@
 TEST(Immediate, imm8)
 {
     imm8 v0 { 0 };
-    EXPECT_EQ(v0.to_str(), "0x00");
+    EXPECT_EQ(v0.to_str(), "$0x00");
 
     imm8 v1 { 255 };
-    EXPECT_EQ(v0.to_str(), "0xFF");
+    EXPECT_EQ(v1.to_str(), "$0xFF");
+}
+
+TEST(Immediate, imm16)
+{
+    imm16 v0 { 0 };
+    EXPECT_EQ(v0.to_str(), "$0x0000");
+
+    imm16 v1 { 65535 };
+    EXPECT_EQ(v1.to_str(), "$0xFFFF");
 }
