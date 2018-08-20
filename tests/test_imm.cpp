3214d31c43b71fed314a8b0f5634a49221ef1679
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-20 22:39:33 +0200

    imm: added test for imm32

--- tests/test_imm.cpp
+++ tests/test_imm.cpp
@@ -20,3 +20,12 @@ TEST(Immediate, imm16)
     imm16 v1 { 65535 };
     EXPECT_EQ(v1.to_str(), "$0xFFFF");
 }
+
+TEST(Immediate, imm32)
+{
+    imm32 v0 { 0 };
+    EXPECT_EQ(v0.to_str(), "$0x00000000");
+
+    imm32 v1 { 0xFFFFFFFF };
+    EXPECT_EQ(v1.to_str(), "$0xFFFFFFFF");
+}
